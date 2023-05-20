51cab29a07fecec1f6f7d3a03ecb7c9ecfad35c4
Skoles <skoles27@gmail.com>
2023-05-20 16:18:59 +0200

    commit 7, add commen and liket to other user page

--- src/app/user/show-user/show-user.component.ts
+++ src/app/user/show-user/show-user.component.ts
@@ -4,6 +4,7 @@ import { Post } from 'src/app/models/Post';
 import { User } from 'src/app/models/User';
 import { CommentService } from 'src/app/service/comment.service';
 import { ImageUploadService } from 'src/app/service/image-upload.service';
+import { NotificationService } from 'src/app/service/notification.service';
 import { PostService } from 'src/app/service/post.service';
 import { UserService } from 'src/app/service/user.service';
 
@@ -27,6 +28,7 @@ export class ShowUserComponent implements OnInit {
     private imgService: ImageUploadService,
     private userService: UserService,
     private commentService: CommentService,
+    private notificationService: NotificationService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -78,6 +80,41 @@ export class ShowUserComponent implements OnInit {
       this.commentService.getAllCommentsToPost(p.id!)
         .subscribe(data =>
           p.comments = data)
+    });
+  }
+
+  likePost(postId: number, postIndex: number): void {
+    const post = this.posts[postIndex];
+    console.log(post);
+
+    if (!post.usersLiked?.includes(this.user.username)) {
+      this.postService.likePost(postId, this.user.username)
+        .subscribe(() => {
+          post.likes! ++;
+          post.usersLiked?.push(this.user.username);
+          this.notificationService.showSnackBar('Liked!');
+        });
+    } else {
+      this.postService.likePost(postId, this.user.username)
+        .subscribe(() => {
+          const index = post.usersLiked?.indexOf(this.user.username, 0);
+          if (index! > -1) {
+            post.likes! --;
+            post.usersLiked?.splice(index!, 1);
+            this.notificationService.showSnackBar('Like removed!');
+          }
+        });
+    }
+  }
+
+  postComment(message: string, postId: number, postIndex: number): void {
+    const post = this.posts[postIndex];
+
+    console.log(post);
+    this.commentService.createComment(postId, message).subscribe(data => {
+      console.log(data);
+      post.comments?.push(data);
+      this.notificationService.showSnackBar('Comment published!');
     });
   }
 
